6822fe491808d098f78f21f6e93330251493dc33
OLDYAN <dlyang@ucdavis.edu>
2021-09-27 14:39:43 -0700

    Created week2 homework

---
+++ scripts/Week2_Homework.R
@@ -0,0 +1,14 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+#Problem 1. 
+#Take your hw2 vector and removed all the NAs then select all the numbers between 14 and 38 inclusive, call this vector prob1.
+
+
+#Problem 2.
+#Multiply each number in the prob1 vector by 3 to create a new vector called times3. Then add 10 to each number in your times3 vector to create a new vector called plus10.
+
+
+#Problem 3.
+#Select every other number in your plus10 vector by selecting the first number, not the second, the third, not the fourth, etc. 
