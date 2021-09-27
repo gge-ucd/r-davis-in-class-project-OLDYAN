c9fd26638d33a0504853a1678448d2a7ceef3810
OLDYAN <dlyang@ucdavis.edu>
2021-09-27 15:47:59 -0700

    Finished week2 homework

--- scripts/Week2_Homework.R
+++ scripts/Week2_Homework.R
@@ -4,11 +4,21 @@ hw2 <- replace(hw2, c(4,12,22,27), NA)
 hw2
 #Problem 1. 
 #Take your hw2 vector and removed all the NAs then select all the numbers between 14 and 38 inclusive, call this vector prob1.
-
-
+prob1 = hw2[complete.cases(hw2) & hw2>=14 & hw2<=38]
+prob1
+#
 #Problem 2.
 #Multiply each number in the prob1 vector by 3 to create a new vector called times3. Then add 10 to each number in your times3 vector to create a new vector called plus10.
-
-
+times3 = prob1*3
+times3
+plus10 = times3+10
+plus10
+#
 #Problem 3.
-#Select every other number in your plus10 vector by selecting the first number, not the second, the third, not the fourth, etc. 
+#Select every other number in your plus10 vector by selecting the first number, not the second, the third, not the fourth, etc.
+final = plus10[c(T,F)]
+final
+#
+final = final[seq(1,length(final),2)]
+final
+#
